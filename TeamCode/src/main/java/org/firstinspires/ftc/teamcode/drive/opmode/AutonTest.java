37644b41cf54eb890e6383463f1cdc8770e39880
raypratt <ray.pratt@jacksonpec.org>
2022-12-09 13:43:54 -0500

    1.0

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutonTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+@Config
+@Autonomous
+public class AutonTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode(){
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        waitForStart();
+
+        Pose2d startPose = new Pose2d(0, 0, Math.toRadians(0));
+
+        drive.setPoseEstimate(startPose);
+
+        Trajectory traj1 = drive.trajectoryBuilder(startPose)
+                .splineTo(new Vector2d(-41, 43), Math.toRadians(0))
+                .build();
+
+
+        drive.followTrajectory(traj1);
+
+    }
+}
