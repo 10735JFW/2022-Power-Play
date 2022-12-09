ce1c20819d019c616ea4334e0879a6472b8c0a17
kontech999 <kontech999@gmail.com>
2022-12-09 14:02:38 -0500

    Update DriveConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -32,7 +32,7 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(40, 0, 20,
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(040, 0, 20,
             17.11);
 
     /*
